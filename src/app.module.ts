aa495b0f0d3f47824f2e8c524178a1b35caa9ba3
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2025-09-03 13:25:55 -0300

    feat: criado os recursos de livros, autores e categorias

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthorModule } from './author/author.module';
+import { CategoryModule } from './category/category.module';
+import { BookModule } from './book/book.module';
+
+@Module({
+  imports: [AuthorModule, CategoryModule, BookModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
